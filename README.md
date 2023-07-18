4183771e01496eb14645cd0469ada80ed75bcb3c
Gl Mus <glebmusalev@mail.ru>
2023-07-18 15:23:08 +0300

    Install and connect postgresql

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Django project | Team 28
+Подключение Postgresql:
+
+1. Устанавливаем СУБД 'sudo apt install postgresql'
+2. Подключаемся к СУБД коммандой 'sudo -i -u postgres'
+3. Вводим команду 'psql'
+4. Создаем пользователя "CREATE USER username WITH PASSWORD 'postgres'"
+5. Наделяем пользователя правами суперпользователя командой 'ALTER USER username WITH SUPERUSER'
+6. Создаем базу данных для проекта командой 'createdb store_data'
+7. Переходим в папку проекта и подключаем зависимости файла requirements.txt командой 'pip install -r requirements'
+8. В файле store/settings.py находим подключение к базе данных 
+```python
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.sqlite3',
+      'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
+   }
+}
+```
+и меняем его на 
+```python
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'store_data',
+        'USER': 'username',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+    }
+}
+```
+9. Выполняем миграции командой 'python manage.py migrate'
--- store/settings.py
+++ store/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = "django-insecure-c3_&8ka(lyq$lq8w(zu#y%_jd4e2n#pvv%l)z$4h)t%tk44yq_
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +67,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "store.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'store_data',
+        'USER': 'username',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -99,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -110,7 +108,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
