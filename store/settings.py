841e24e9045bcb6db8569514a0578a160bbc2d7d
Gl Mus <glebmusalev@mail.ru>
2023-07-20 13:57:36 +0300

    Install and connect postgresql try 3

--- store/settings.py
+++ store/settings.py
@@ -71,13 +71,13 @@ WSGI_APPLICATION = "store.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'store_db',
-        'USER': 'store_username',
-        'PASSWORD': '0xA6JtA2cxMdyGxYwc6p',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "store_db",
+        "USER": "store_username",
+        "PASSWORD": "0xA6JtA2cxMdyGxYwc6p",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
