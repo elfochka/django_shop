1ef30274b61df29024e1dbb2bb166c278b95cc46
OlyaNaumovskaya <olga.naumouskaya@gmail.com>
2023-07-26 23:09:57 +0200

    Add work with media-files

--- store/urls.py
+++ store/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from products.views import IndexView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -29,3 +30,6 @@ urlpatterns = [
     path("users/", include("users.urls")),
     path("core/", include("core.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
