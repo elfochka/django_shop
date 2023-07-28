0a3bac709cbeb83af2af64d13a35606298016248
hazadus <hazadus7@gmail.com>
2023-07-29 02:57:48 +0300

    dev: Show ProductImage inlines in Product admin view.

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
 
 from .models import Category, Tag, Product, ProductImage
 
@@ -34,6 +37,56 @@ class TagAdmin(admin.ModelAdmin):
     ]
 
 
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    """
+    Configures admin panel views for ProductImage.
+    """
+
+    model = ProductImage
+    list_display = [
+        "title",
+        "product",
+    ]
+    ordering = [
+        "product",
+        "id",
+    ]
+
+
+class ProductImageInline(TabularInline):
+    """
+    Inline ProductImage row with built-in image preview.
+    """
+
+    model = ProductImage
+    fields = [
+        "image_preview",
+        "title",
+        "image",
+    ]
+    readonly_fields = ["image_preview"]
+    ordering = ["id"]
+    extra = 0
+
+    def image_preview(self, product_image: ProductImage) -> str:
+        """
+        Render image preview.
+        """
+        if product_image.image:
+            return mark_safe(
+                render_to_string(
+                    "products/product_image_preview.html",
+                    {
+                        "product_image": product_image,
+                    },
+                )
+            )
+        return ""
+
+    image_preview.short_description = "Превью"
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     """
@@ -57,6 +110,7 @@ class ProductAdmin(admin.ModelAdmin):
         "created",
         "updated",
     ]
+    inlines = [ProductImageInline]
     fieldsets = [
         (
             "Основная информация о товаре",
@@ -96,21 +150,4 @@ class ProductAdmin(admin.ModelAdmin):
                 ]
             },
         ),
-    ]
-
-
-@admin.register(ProductImage)
-class ProductImageAdmin(admin.ModelAdmin):
-    """
-    Configures admin panel views for ProductImage.
-    """
-
-    model = ProductImage
-    list_display = [
-        "title",
-        "product",
-    ]
-    ordering = [
-        "product",
-        "id",
     ]
