b0a7cc3bc31959e0b954bffb71ea8c95e48c8e54
hazadus <hazadus7@gmail.com>
2023-07-29 00:41:43 +0300

    dev: Render category list in categories dropdown.

--- products/views.py
+++ products/views.py
@@ -1,21 +1,36 @@
 from django.views.generic import TemplateView
 
+from .models import Category
 
-class IndexView(TemplateView):
+
+class BaseView(TemplateView):
+    """
+    Put data necessary for base.html template into context.
+    """
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.filter(
+            parent=None, is_deleted=False
+        ).order_by("pk")
+        return context
+
+
+class IndexView(BaseView):
     template_name = "index.html"
 
 
-class CatalogView(TemplateView):
+class CatalogView(BaseView):
     template_name = "products/catalog.html"
 
 
-class ProductDetailsView(TemplateView):
+class ProductDetailsView(BaseView):
     template_name = "products/product.html"
 
 
-class CompareView(TemplateView):
+class CompareView(BaseView):
     template_name = "products/compare.html"
 
 
-class SaleView(TemplateView):
+class SaleView(BaseView):
     template_name = "products/sale.html"
