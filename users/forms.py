dd0535e574795fd83c9334d565804a94383c2a22
Maxim Platonov <bliamba2000@gmail.com>
2023-08-07 16:56:56 +0400

    Changed class CustomUserChangeForm

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-
+from django import forms
 from .models import CustomUser
 
 
@@ -10,6 +10,8 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class CustomUserChangeForm(UserChangeForm):
+    full_name = forms.CharField(max_length=255, label="ФИО")
+
     class Meta:
         model = CustomUser
-        fields = ("image", "email", "phone", "username")
+        fields = ("image", "email", "phone", "first_name", "middle_name", "last_name")
