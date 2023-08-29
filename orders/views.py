9d9977a171de714e3c9ea76cacf090f8468bedfc
hazadus <hazadus7@gmail.com>
2023-08-29 18:47:50 +0300

    dev: Checkout template split to 4 steps.

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,20 @@
 from django.views.generic import TemplateView
 
+from products.views import BaseMixin
+
 
 class CartView(TemplateView):
     template_name = "orders/cart.html"
 
 
-class CheckoutView(TemplateView):
+class CheckoutView(BaseMixin, TemplateView):
     template_name = "orders/checkout.html"
+
+    def get_context_data(self, **kwargs):
+        """Put step number into context."""
+        context = super().get_context_data(**kwargs)
+        context["step"] = self.request.GET.get("step", "1")
+        return context
 
 
 class PaymentView(TemplateView):
