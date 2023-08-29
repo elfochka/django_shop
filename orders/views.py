505842763ed835855b37c81d97cc3ac5a9c1bb29
hazadus <hazadus7@gmail.com>
2023-08-29 23:52:06 +0300

    refactor: DRY view code.

--- orders/views.py
+++ orders/views.py
@@ -13,6 +13,12 @@ class CartView(TemplateView):
 class CheckoutView(BaseMixin, FormView):
     template_name = "orders/checkout.html"
     form_class = CheckoutStep1
+    # Form classes for each step
+    form_classes = {
+        "1": CheckoutStep1,
+        "2": CheckoutStep2,
+        "3": CheckoutStep3,
+    }
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -28,6 +34,7 @@ class CheckoutView(BaseMixin, FormView):
         return context
 
     def get_form(self, form_class=None):
+        """Fill form for current step with initial values taken from `order` dict in the session."""
         if self.request.method == "GET":
             # TODO: set form defaults for all steps using session data
             if self.request.GET.get("step") == "1":
@@ -56,50 +63,31 @@ class CheckoutView(BaseMixin, FormView):
         return super().get_form(form_class)
 
     def get(self, request, *args, **kwargs):
-        step = self.request.GET.get("step")
-
-        # TODO: refactor using dictionary
-        if step == "1":
-            self.form_class = CheckoutStep1
-
-        if step == "2":
-            self.form_class = CheckoutStep2
-
-        if step == "3":
-            self.form_class = CheckoutStep3
+        """Set form for current step."""
+        step = self.request.GET.get("step", "1")
+        if step in self.form_classes.keys():
+            self.form_class = self.form_classes[step]
 
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        step = self.request.GET.get("step")
+        """Save input from the form into `order` dict in the session."""
+        step = self.request.GET.get("step", "1")
         # Save step value for use in `get_success_url()`
         self.step = step
 
+        # Get order dict from session, create it if it not exists yet
         order = request.session.get(settings.ORDER_SESSION_ID, "")
         if not order:
-            request.session[settings.ORDER_SESSION_ID] = {}
+            order = request.session[settings.ORDER_SESSION_ID] = {}
 
-        if step == "1":
-            self.form_class = CheckoutStep1
-            form = CheckoutStep1(request.POST)
+        # Use form for the current step to validate input, and store it in session
+        if step in self.form_classes.keys():
+            self.form_class = self.form_classes[step]
+            form = self.form_classes[step](request.POST)
             if form.is_valid():
-                order["name"] = form.cleaned_data["name"]
-                order["phone"] = form.cleaned_data["phone"]
-                order["email"] = form.cleaned_data["email"]
-
-        if step == "2":
-            self.form_class = CheckoutStep2
-            form = CheckoutStep2(request.POST)
-            if form.is_valid():
-                order["delivery"] = form.cleaned_data["delivery"]
-                order["city"] = form.cleaned_data["city"]
-                order["address"] = form.cleaned_data["address"]
-
-        if step == "3":
-            self.form_class = CheckoutStep3
-            form = CheckoutStep3(request.POST)
-            if form.is_valid():
-                order["payment"] = form.cleaned_data["payment"]
+                for key in form.cleaned_data.keys():
+                    order[key] = form.cleaned_data[key]
 
         request.session.modified = True
         return super().post(request, *args, **kwargs)
